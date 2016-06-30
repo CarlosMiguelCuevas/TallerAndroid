4bd95b912656ce847de1953926742317704fa767
CarlosMiguelDesarrollo <carlos.cuevas.desarrollo@gmail.com>
2016-06-30 16:23:49 -0700

    add the detal activity and it layout

---
+++ app/src/main/java/mx/com/cubozsoft/apprentice/Pokemon_detal.java
@@ -0,0 +1,13 @@
+package mx.com.cubozsoft.apprentice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pokemon_detal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_detal);
+    }
+}
