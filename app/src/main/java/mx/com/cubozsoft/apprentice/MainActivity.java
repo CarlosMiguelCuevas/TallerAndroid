5d1b15d7f3d69c20bb4cabda85aab0707655a483
CarlosMiguelDesarrollo <carlos.cuevas.desarrollo@gmail.com>
2016-06-30 17:28:59 -0700

    finish

--- app/src/main/java/mx/com/cubozsoft/apprentice/MainActivity.java
+++ app/src/main/java/mx/com/cubozsoft/apprentice/MainActivity.java
@@ -1,6 +1,7 @@
 package mx.com.cubozsoft.apprentice;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,24 +28,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        int id_image = 0;
+        String desc = "";
+        String name = "";
 
         switch (v.getId()){
             case R.id.pokelist_1:
-//                Toast.makeText(this,getString(R.string.pokemon1_name),Toast.LENGTH_SHORT).show();
+                id_image = R.drawable.bolba;
+                desc = getString(R.string.pokemon1_desc);
+                name = getString(R.string.pokemon1_name);
                 break;
+
             case R.id.pokelist_2:
-//                Toast.makeText(this,getString(R.string.pokemon2_name),Toast.LENGTH_SHORT).show();
+                id_image = R.drawable.squirtle;
+                desc = getString(R.string.pokemon2_desc);
+                name = getString(R.string.pokemon2_name);
                 break;
+
             case R.id.pokelist_3:
-//                Toast.makeText(this,getString(R.string.pokemon3_name),Toast.LENGTH_SHORT).show();
+                id_image = R.drawable.charmander;
+                desc = getString(R.string.pokemon3_desc);
+                name = getString(R.string.pokemon3_name);
                 break;
+
             case R.id.pokelist_4:
-//                Toast.makeText(this,getString(R.string.pokemon4_name),Toast.LENGTH_SHORT).show();
+                id_image = R.drawable.pika;
+                desc = getString(R.string.pokemon4_desc);
+                name = getString(R.string.pokemon4_name);
                 break;
         }
 
         Intent detailIntent = new Intent(this,Pokemon_detal.class);
+        detailIntent.putExtra(Pokemon_detal.IMG_DETAIL,id_image);
+        detailIntent.putExtra(Pokemon_detal.NAME_DETAIL,name);
+        detailIntent.putExtra(Pokemon_detal.DESC_DETAIL,desc);
+
         startActivity(detailIntent);
     }
 }
--- app/src/main/java/mx/com/cubozsoft/apprentice/Pokemon_detal.java
+++ app/src/main/java/mx/com/cubozsoft/apprentice/Pokemon_detal.java
@@ -2,12 +2,30 @@ package mx.com.cubozsoft.apprentice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Pokemon_detal extends AppCompatActivity {
+    public static final String IMG_DETAIL = "pokeImage";
+    public static final String NAME_DETAIL = "pokeName";
+    public static final String DESC_DETAIL = "pokeDesc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_detal);
+
+        ImageView photo = (ImageView) findViewById(R.id.pokeImg_detail);
+        TextView name = (TextView) findViewById(R.id.pokeName_detail);
+        TextView desc = (TextView) findViewById(R.id.pokeDesc_detail);
+
+        Bundle info = getIntent().getExtras();
+
+        int id = info.getInt(IMG_DETAIL);
+
+
+        photo.setImageResource(id);
+        name.setText(info.getString(NAME_DETAIL));
+        desc.setText(info.getString(DESC_DETAIL));
     }
 }
